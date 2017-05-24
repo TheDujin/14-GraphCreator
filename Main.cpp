155753d0fcdc1200dd9ebca374ef41d0f98aafe4
U-BSD\293359 <293359@SHS-10DD3C387.beaverton.k12.or.us>
2017-05-24 14:55:53 -0700

    I think I'm finished, I just need comments and to remove debug statements

--- Main.cpp
+++ Main.cpp
@@ -21,6 +21,8 @@ void addVertex(vector<Vertex*>* graph, Vertex* newVertex);
 void addEdge(Vertex* firstNode, Vertex* secondNode, int weight, int** adjacencyMatrix);
 void removeVertex(vector<Vertex*>* graph, int** adjacencyMatrix);
 void removeEdge(Vertex* firstNode, Vertex* secondNode, int** adjacencyMatrix);
+char findNode(vector<Vertex*>* graph, int IDTarget);
+int findPath(Vertex* firstNode, Vertex* secondNode, int** adjacencyMatrix, int* shortestLength);
 
 
 int main() {
@@ -42,13 +44,7 @@ int main() {
 		for (int i = 0; i < graphPtr->size(); i++) {
 			cout << graphPtr->at(i)->label << " " << graphPtr->at(i)->trueID << endl;
 		}
-		for (int i = 0; i < 20; i++) {
-				for (int j = 0; j < 20; j++) {
-					cout << adjacencyMatrix[i][j] << " ";
-				}
-				cout << endl;
-			}
-		cout << "Would you like to \"ADD\" something, \"REMOVE\" something, or \"QUIT\" the program?" << endl << "?: ";
+		cout << "Would you like to \"ADD\" something, \"REMOVE\" something, \"DISPLAY\" the adjacency matrix, \"SEARCH\" for a path between two nodes, or \"QUIT\" the program?" << endl << "?: ";
 		cin >> choose;
 		cin.ignore(256, '\n');
 		if (choose == 'a' || choose == 'A') {
@@ -138,6 +134,61 @@ int main() {
 				cout << "That input is invalid. Please try again." << endl;
 			}
 		}
+		else if (choose == 'd' || choose == 'D') {
+			cout << "Here is the adjacency matrix:" << endl;
+			cout << " \t";
+			for (int i = 0; i < 20; i++) {
+				cout << findNode(graphPtr, i) << '\t';
+			}
+			cout << endl;
+			for (int i = 0; i < 20; i++) {
+				cout << findNode(graphPtr, i) << '\t';
+					for (int j = 0; j < 20; j++) {
+						cout << adjacencyMatrix[i][j] << '\t';
+					}
+					cout << endl;
+				}
+		}
+		else if (choose == 's' || choose == 'S') {
+			cout << "Enter the label of the starting vertex." << endl << "Label: ";
+			char firstChoose;
+			cin >> firstChoose;
+			Vertex* first = NULL;
+			for (int i = 0; i < graphPtr->size(); i++) {
+				if (graphPtr->at(i)->label == firstChoose) {
+					first = graphPtr->at(i);
+				}
+			}
+			if (first == NULL) {
+				cout << "A vertex with that label does not exist. Please try again." << endl;
+				continue;
+			}
+			cout << "Enter the label of the ending vertex." << endl << "Label: ";
+			char secondChoose;
+			cin >> secondChoose;
+			Vertex* second = NULL;
+			for (int i = 0; i < graphPtr->size(); i++) {
+				if (graphPtr->at(i)->label == secondChoose) {
+					second = graphPtr->at(i);
+				}
+			}
+			if (second == NULL) {
+				cout << "A vertex with that label does not exist. Please try again." << endl;
+				continue;
+			}
+			int* shortestLength = new int[20];
+			for (int i = 0; i < 20; i++) {
+				shortestLength[i] = -1;
+			}
+			shortestLength[first->trueID] = 0;
+			int length = findPath(first, second, adjacencyMatrix, shortestLength);
+			if (length > -1) {
+				cout << "The shortest path between vertex " << firstChoose << " and vertex " << secondChoose << " is " << length << "." << endl;
+			}
+			else {
+				cout << "There is no path between vertex " << firstChoose << " and vertex " << secondChoose << "." << endl;
+			}
+		}
 		else if (choose == 'q' || choose == 'Q') {
 			cout << "Thanks for using this program!" << endl;
 			running = false;
@@ -156,8 +207,16 @@ void addVertex(vector<Vertex*>* graph, Vertex* newVertex) {
 	for (int i = 0; i < 20; i++) {
 		whatExists[i] = false;
 	}
+	bool labelAlreadyUsed = false;
 	for (int i = 0; i < graph->size(); i++) {
 		whatExists[graph->at(i)->trueID] = true;
+		if (graph->at(i)->label == newVertex->label) {
+			labelAlreadyUsed = true;
+		}
+	}
+	if (labelAlreadyUsed) {
+		cout << "A vertex with that label already exists in the graph! Please try again." << endl;
+		return;
 	}
 	for (int i = 0; i < 20; ++i) {
 		if (whatExists[i] == false) {
@@ -194,8 +253,8 @@ void removeVertex(vector<Vertex*>* graph, int** adjacencyMatrix) {
 	}
 	int index = target->trueID;
 	for (int i = 0; i < 20; i++) {
-		adjacencyMatrix[i][index];
-		adjacencyMatrix[index][i];
+		adjacencyMatrix[i][index] = -1;
+		adjacencyMatrix[index][i] = -1;
 	}
 	cout << "The vertex with label " << target->label << " was removed." << endl;
 	graph->erase(graph->begin() + targetIndex);
@@ -207,4 +266,34 @@ void removeEdge(Vertex* firstNode, Vertex* secondNode, int** adjacencyMatrix) {
 	int secondIndex = secondNode->trueID;
 	adjacencyMatrix[firstIndex][secondIndex] = -1;
 	cout << "Edge successfully removed from vertex " << firstNode->label << " to vertex " << secondNode->label << "." << endl;
+}
+
+char findNode(vector<Vertex*>* graph, int IDTarget) {
+	for (int i = 0; i < graph->size(); i++) {
+		if (graph->at(i)->trueID == IDTarget) {
+			return graph->at(i)->label;
+		}
+	}
+	return ' ';
+}
+
+int findPath(Vertex* firstNode, Vertex* secondNode, int** adjacencyMatrix, int* shortestLength) {
+	bool foundNew = true;
+	while (foundNew) {
+		foundNew = false;
+		for (int i = 0; i < 20; i++) {
+			if (shortestLength[i] > -1) {
+				for (int j = 0; j < 20; j++) {
+					if (adjacencyMatrix[i][j] > -1) {
+						int testPath = shortestLength[i] + adjacencyMatrix[i][j];
+						if (shortestLength[j] == -1 || testPath < shortestLength[j]) {
+							shortestLength[j] = testPath;
+							foundNew = true;
+						}
+					}
+				}
+			}
+		}
+	}
+	return shortestLength[secondNode->trueID];
 }
